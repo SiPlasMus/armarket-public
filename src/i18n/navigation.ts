0495b9a4d075fd5d3816346a7b1dfce57bfc72dd
siplasmus <tim973785464@mail.ru>
2026-03-27 20:48:35 +0500

    V4 design. improving pages, more elements.

--- src/i18n/navigation.ts
+++ src/i18n/navigation.ts
@@ -1,4 +1,16 @@
+import { createElement, forwardRef } from 'react';
 import { createNavigation } from 'next-intl/navigation';
 import { routing } from './routing';
 
-export const { Link, redirect, usePathname, useRouter } = createNavigation(routing);
+const navigation = createNavigation(routing);
+const BaseLink = navigation.Link;
+
+export const redirect = navigation.redirect;
+export const usePathname = navigation.usePathname;
+export const useRouter = navigation.useRouter;
+
+type LinkProps = React.ComponentProps<typeof BaseLink>;
+
+export const Link = forwardRef<HTMLAnchorElement, LinkProps>(function Link(props, ref) {
+  return createElement(BaseLink, { ...props, ref, prefetch: false });
+});
